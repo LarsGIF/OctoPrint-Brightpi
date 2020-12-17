b19fc9e5981fa306eb56ec44cdd7707d2c1f7b85
LarsGIF <lars.fransen@telia.com>
2020-12-17 16:55:11 +0100

    Updated import statement in _init_.py

--- octoprint_brightpi/__init__.py
+++ octoprint_brightpi/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 import octoprint.plugin
-from camera_light import CameraLight
+from .camera_light import *
 
 camera = CameraLight()
 
--- octoprint_brightpi/camera_light.py
+++ octoprint_brightpi/camera_light.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from brightpilib import *
 
 class CameraLight(object):
